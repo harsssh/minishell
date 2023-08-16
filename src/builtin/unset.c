be77894e79422f9188bcdc9c1a138fa0a961c8ad
harsh <66548698+harsssh@users.noreply.github.com>
2023-08-17 07:11:16 +0900

    implement builtin unset

---
+++ src/builtin/unset.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 06:55:08 by kemizuki          #+#    #+#             */
+/*   Updated: 2023/08/17 06:55:09 by kemizuki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtin.h"
+#include "builtin_internal.h"
+#include "variable.h"
+#include <stdlib.h>
+
+static char	**skip_options(char **args)
+{
+	int	c;
+
+	reset_get_next_option();
+	while (true)
+	{
+		c = get_next_option(*args, "");
+		if (c == END_OF_OPTIONS)
+			break ;
+		if (c == END_OF_ARG)
+			++args;
+	}
+	return (args);
+}
+
+int	builtin_unset(t_context *ctx, char **args)
+{
+	args = skip_options(args);
+	while (*args != NULL)
+	{
+		unsetvar(ctx, *args);
+		++args;
+	}
+	return (EXIT_SUCCESS);
+}
