c098a7cb7679786dafd9687e536640cd7266db95
harsh <66548698+harsssh@users.noreply.github.com>
2023-08-18 22:37:15 +0900

    env: fix and add test

--- test/builtin_env.cpp
+++ test/builtin_env.cpp
@@ -38,3 +38,23 @@ TEST(builtin_env, no_env) {
 	auto output = testing::internal::GetCapturedStdout();
 	ASSERT_STREQ(output.c_str(), "");
 }
+
+// NO_VALUE属性がついた環境変数は出力しない
+TEST(builtin_env, no_value) {
+	t_context ctx;
+
+	ctx.variables = ft_list_create();
+	setvar(&ctx, "NAME1", "VALUE1", 1);
+	setvar(&ctx, "NAME2", "VALUE2", 1);
+	// 属性を設定
+	auto var = getvar(&ctx, "NAME1");
+	var->attributes |= VAR_ATTR_EXPORTED | VAR_ATTR_NO_VALUE;
+	var = getvar(&ctx, "NAME2");
+	var->attributes |= VAR_ATTR_EXPORTED;
+
+	testing::internal::CaptureStdout();
+	char *args[] = {nullptr};
+	builtin_env(&ctx, args);
+	auto output = testing::internal::GetCapturedStdout();
+	ASSERT_STREQ(output.c_str(), "NAME2=VALUE2\n");
+}
