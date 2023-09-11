c030aa8507f0efe033f6a598433861e824771791
harsh <66548698+harsssh@users.noreply.github.com>
2023-09-11 17:43:01 +0900

    libft norm

--- libft/include/ft_string.h
+++ libft/include/ft_string.h
@@ -6,7 +6,7 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 01:01:27 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/07/31 00:42:32 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/09/11 17:36:27 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	**ft_split(char const *s, char c);
 int		ft_strall(const char *str, int (*f)(int));
 int		ft_strany(const char *str, int (*f)(int));
 char	*ft_strchr(const char *s, int c);
-char	*ft_strcpy(char * dst, const char * src);
+char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strdup(const char *src);
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_strjoin(char const *s1, char const *s2);
--- libft/string/ft_strcpy.c
+++ libft/string/ft_strcpy.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 17:36:40 by kemizuki          #+#    #+#             */
+/*   Updated: 2023/09/11 17:36:41 by kemizuki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_string.h"
 
-char *ft_strcpy(char * dst, const char * src)
+char	*ft_strcpy(char *dst, const char *src)
 {
-	char *ret;
+	char	*ret;
 
 	ret = dst;
 	while (*src)
