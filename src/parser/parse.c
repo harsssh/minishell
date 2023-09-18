34acaac4227bcd1e7db2e50b74ebc1343d66b93e
Sota Matsuo <smatsuo@c1r26s2.42tokyo.jp>
2023-09-18 21:27:21 +0900

    fix

--- src/parser/parse.c
+++ src/parser/parse.c
@@ -6,7 +6,7 @@
 /*   By: smatsuo <smatsuo@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 20:01:43 by smatsuo           #+#    #+#             */
-/*   Updated: 2023/09/18 20:18:56 by smatsuo          ###   ########.fr       */
+/*   Updated: 2023/09/18 21:24:46 by smatsuo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@
 
 t_ast_node	*parse(t_token_stream *stream)
 {
-	t_parser	*parser;
-
-	parser = new_parser(stream);
-	if (parser == NULL)
-		return (NULL);
-	return (parse_and_or(parser));
+	(void)stream;
+	return (NULL);
+//	t_parser	*parser;
+//
+//	parser = new_parser(stream);
+//	if (parser == NULL)
+//		return (NULL);
+//	return (parse_and_or(parser));
 }
