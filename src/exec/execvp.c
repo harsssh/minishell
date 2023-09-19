3a7192e566993d4a48207956b4c5d7aaba7abd59
harsh <66548698+harsssh@users.noreply.github.com>
2023-09-19 23:10:20 +0900

    wip

--- src/exec/execvp.c
+++ src/exec/execvp.c
@@ -6,12 +6,14 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 19:07:18 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/09/19 19:07:26 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/09/19 22:19:04 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec_internal.h"
+#include "ft_string.h"
 #include "variables.h"
+#include "utils.h"
 #include <unistd.h>
 
 int	internal_execvp(t_context *ctx, const char *file, char *const *argv)
@@ -29,5 +31,8 @@ int	internal_execvp(t_context *ctx, const char *file, char *const *argv)
 		execve(file, argv, envp);
 		return (-1);
 	}
+	path_array = ft_split(var->value, ':');
+	if (path_array == NULL)
+		destroy_string_array(envp);
 	return (0);
 }
