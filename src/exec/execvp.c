0d41921f848e885d585485638dd49aa4fa3af31a
harsh <66548698+harsssh@users.noreply.github.com>
2023-09-20 16:37:21 +0900

    wip

--- src/exec/execvp.c
+++ src/exec/execvp.c
@@ -16,23 +16,28 @@
 #include "utils.h"
 #include <unistd.h>
 
+static int execute_from_path(char **path_array, const char *file, char *const *argv)
+{
+}
+
 int	internal_execvp(t_context *ctx, const char *file, char *const *argv)
 {
 	t_variable	*var;
 	char		**envp;
 	char		**path_array;
 
+	if (file == NULL || *file == '\0')
+		return (-1);
 	envp = get_environ(ctx);
 	if (envp == NULL)
 		return (-1);
+	if (!is_absolute_path(file))
+		return execve(file, argv, envp);
 	var = getvar(ctx, "PATH");
 	if (var == NULL)
-	{
-		execve(file, argv, envp);
-		return (-1);
-	}
+		return execve(file, argv, envp);
 	path_array = ft_split(var->value, ':');
 	if (path_array == NULL)
 		destroy_string_array(envp);
-	return (0);
+	return (-1);
 }
