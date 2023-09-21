793c2d66e30f28b32a33c6a66aed4cc81d8f7ffe
harsh <66548698+harsssh@users.noreply.github.com>
2023-09-21 20:20:10 +0900

    handle and/or node

--- src/exec/handle_and_or.c
+++ src/exec/handle_and_or.c
@@ -6,16 +6,18 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 00:10:29 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/09/21 00:10:30 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/09/21 20:19:54 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec_internal.h"
+#include <stdlib.h>
 
 int	handle_and_or(t_context *ctx, t_pipeline_info *info, t_ast_node *ast)
 {
-	(void)ctx;
-	(void)info;
-	(void)ast;
-	return (0);
+	execute_ast_impl(ctx, info, ast->left);
+	if ((ast->type == N_AND && ctx->last_exit_status == EXIT_SUCCESS)
+		|| (ast->type == N_OR && ctx->last_exit_status != EXIT_SUCCESS))
+		execute_ast_impl(ctx, info, ast->right);
+	return (EXIT_SUCCESS);
 }
