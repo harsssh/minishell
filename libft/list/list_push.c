02a5da7853e8f0735e125bfbc9d4f1de831f8982
Sota Matsuo <smatsuo@c1r27s3.42tokyo.jp>
2023-09-22 14:10:09 +0900

    refactor return value of push_front

--- libft/list/list_push.c
+++ libft/list/list_push.c
@@ -6,22 +6,22 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 07:22:52 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/09/22 13:58:22 by smatsuo          ###   ########.fr       */
+/*   Updated: 2023/09/22 14:07:58 by smatsuo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 #include "list_internal.h"
 
-void	ft_list_push_front(t_list *list, void *data)
+t_node	*ft_list_push_front(t_list *list, void *data)
 {
 	t_node	*node;
 
 	if (list == NULL)
-		return ;
+		return (NULL);
 	node = ft_node_create(data);
 	if (node == NULL)
-		return ;
+		return (NULL);
 	node->next = list->head;
 	if (list->head != NULL)
 		list->head->prev = node;
@@ -29,6 +29,7 @@ void	ft_list_push_front(t_list *list, void *data)
 	if (list->tail == NULL)
 		list->tail = node;
 	++list->size;
+	return (node);
 }
 
 t_node	*ft_list_push_back(t_list *list, void *data)
