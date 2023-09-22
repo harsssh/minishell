ac9728c4ca3e543ee1f6924e1ad77fc85c5ad500
harsh <66548698+harsssh@users.noreply.github.com>
2023-09-23 03:19:20 +0900

    support builtin

--- src/exec/exec_internal.h
+++ src/exec/exec_internal.h
@@ -6,7 +6,7 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 18:35:21 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/09/22 15:27:03 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/09/23 03:18:29 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,6 @@ void			wait_children(t_context *ctx, t_list *child_pid_list);
 bool			is_in_pipeline(t_pipeline_info *info);
 
 // configure_io.c
-void	configure_io(t_list *redirect_list, t_pipeline_info *info);
+void			configure_io(t_list *redirect_list, t_pipeline_info *info);
 
 #endif
--- src/exec/handle_command.c
+++ src/exec/handle_command.c
@@ -6,22 +6,23 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 00:10:48 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/09/22 21:31:15 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/09/23 03:18:38 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ast.h"
+#include "builtins.h"
 #include "context.h"
 #include "exec_internal.h"
-#include "builtins.h"
 #include "ft_list.h"
 #include "utils.h"
 #include <stdlib.h>
 
-static int call_builtin_func(t_context *ctx, t_list *argv_list, t_builtin_func func)
+static int	call_builtin_func(t_context *ctx, t_list *argv_list,
+		t_builtin_func func)
 {
-	char **argv;
-	int ret;
+	char	**argv;
+	int		ret;
 
 	argv = list_to_string_array(argv_list, NULL);
 	if (argv == NULL)
@@ -31,11 +32,12 @@ static int call_builtin_func(t_context *ctx, t_list *argv_list, t_builtin_func f
 	return (ret);
 }
 
-static void execute_command_in_child(t_context *ctx, t_pipeline_info *info, t_ast_node *ast, t_builtin_func func)
+static void	execute_command_in_child(t_context *ctx, t_pipeline_info *info,
+		t_ast_node *ast, t_builtin_func func)
 {
 	pid_t	pid;
 
- 	pid = fork();
+	pid = fork();
 	if (pid == -1)
 		return ;
 	if (pid == 0)
@@ -52,14 +54,25 @@ static void execute_command_in_child(t_context *ctx, t_pipeline_info *info, t_as
 		wait_children(ctx, info->child_pid_list);
 }
 
-static void execute_builtin(t_context *ctx, t_pipeline_info *info, t_ast_node *ast, t_builtin_func func)
+static void	execute_builtin(t_context *ctx, t_pipeline_info *info,
+		t_ast_node *ast, t_builtin_func func)
 {
+	int	saved_stdin_fd;
+	int	saved_stdout_fd;
 
+	saved_stdin_fd = dup(STDIN_FILENO);
+	saved_stdout_fd = dup(STDOUT_FILENO);
+	configure_io(ast->redirects, info);
+	ctx->last_exit_status = call_builtin_func(ctx, ast->argv, func);
+	dup2(saved_stdin_fd, STDIN_FILENO);
+	dup2(saved_stdout_fd, STDOUT_FILENO);
+	close(saved_stdin_fd);
+	close(saved_stdout_fd);
 }
 
 int	handle_command(t_context *ctx, t_pipeline_info *info, t_ast_node *ast)
 {
-	t_builtin_func func;
+	t_builtin_func	func;
 
 	func = get_builtin_func((char *)ast->argv->head->data);
 	if (func != NULL && !is_in_pipeline(info))
