9784beaba8f81fcd931347ef02175f208b8ff19e
harsh <66548698+harsssh@users.noreply.github.com>
2023-10-15 21:22:07 +0900

    display message if command terminated by signal

---
+++ src/exec/lookup_message.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lookup_message.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/15 20:33:29 by kemizuki          #+#    #+#             */
+/*   Updated: 2023/10/15 21:19:26 by kemizuki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <signal.h>
+
+static void	set_lookup_table_entry(const char **table);
+static void	set_lookup_table_entry2(const char **table);
+
+// TODO: stop initializing the table every time
+const char	*lookup_termination_message(int sig)
+{
+	static const char	*lookup_table[NSIG];
+
+	if (sig <= 0 || sig >= NSIG)
+		return ("Unknown signal");
+	set_lookup_table_entry(lookup_table);
+	return (lookup_table[sig]);
+}
+
+// WARNING: This function is not portable.
+static void	set_lookup_table_entry(const char **table)
+{
+	table[SIGHUP] = "Hangup";
+	table[SIGINT] = "Interrupt";
+	table[SIGQUIT] = "Quit";
+	table[SIGILL] = "Illegal instruction";
+	table[SIGTRAP] = "BPT trace/trap";
+	table[SIGABRT] = "ABORT instruction";
+	table[SIGEMT] = "EMT instruction";
+	table[SIGFPE] = "Floating point exception";
+	table[SIGKILL] = "Killed";
+	table[SIGBUS] = "Bus error";
+	table[SIGSEGV] = "Segmentation fault";
+	table[SIGSYS] = "Bad system call";
+	table[SIGPIPE] = "Broken pipe";
+	table[SIGALRM] = "Alarm clock";
+	table[SIGTERM] = "Terminated";
+	table[SIGURG] = "Urgent IO condition";
+	table[SIGSTOP] = "Stopped (signal)";
+	table[SIGTSTP] = "Stopped";
+	table[SIGCONT] = "Continue";
+	table[SIGCHLD] = "Child death or stop";
+	table[SIGTTIN] = "Stopped (tty input)";
+	table[SIGTTOU] = "Stopped (tty output)";
+	table[SIGIO] = "I/O ready";
+	table[SIGXCPU] = "CPU limit";
+	set_lookup_table_entry2(table);
+}
+
+// WARNING: This function is not portable.
+static void	set_lookup_table_entry2(const char **table)
+{
+	table[SIGXFSZ] = "File limit";
+	table[SIGVTALRM] = "Alarm (virtual)";
+	table[SIGPROF] = "Alarm (profile)";
+	table[SIGWINCH] = "Window changed";
+	table[SIGINFO] = "Information request";
+	table[SIGUSR1] = "User signal 1";
+	table[SIGUSR2] = "User signal 2";
+}
--- src/exec/wait_children.c
+++ src/exec/wait_children.c
@@ -6,15 +6,30 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 19:16:49 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/09/25 02:54:03 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/10/15 21:19:33 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec_internal.h"
-#include "ft_list.h"
+#include "libft.h"
 #include <errno.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <unistd.h>
+
+// NOTE: bash does not display the message for SIGINT
+static void	report_signal_termination(int sig)
+{
+	const char	*message;
+
+	if (sig == SIGINT)
+		write(STDERR_FILENO, "\n", 1);
+	else
+	{
+		message = lookup_termination_message(sig);
+		ft_dprintf(STDERR_FILENO, "%s: %d\n", message, sig);
+	}
+}
 
 static int	get_exit_status(int stat)
 {
@@ -31,17 +46,25 @@ static int	get_exit_status(int stat)
 int	wait_children(pid_t target_pid)
 {
 	int		stat;
-	int		exit_status;
+	int		target_term_sig;
+	int		target_exit_status;
 	pid_t	waited_pid;
 
+	target_term_sig = -1;
+	target_exit_status = -1;
 	errno = 0;
-	exit_status = -1;
 	waited_pid = wait3(&stat, WNOHANG, NULL);
 	while (errno != ECHILD)
 	{
 		if (waited_pid == target_pid)
-			exit_status = get_exit_status(stat);
+		{
+			target_exit_status = get_exit_status(stat);
+			if (WIFSIGNALED(stat))
+				target_term_sig = WTERMSIG(stat);
+		}
 		waited_pid = wait3(&stat, WNOHANG, NULL);
 	}
-	return (exit_status);
+	if (target_term_sig != -1)
+		report_signal_termination(target_term_sig);
+	return (target_exit_status);
 }
