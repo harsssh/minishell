4201a7372c1d40ecd2b43cf52a23b6556d8ee315
harsh <66548698+harsssh@users.noreply.github.com>
2023-10-17 17:24:18 +0900

    display exit message

--- src/main.c
+++ src/main.c
@@ -6,25 +6,35 @@
 /*   By: smatsuo <smatsuo@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 22:28:27 by smatsuo           #+#    #+#             */
-/*   Updated: 2023/10/16 22:45:39 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/10/17 17:23:29 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ast.h"
 #include "context.h"
-#include <parser.h>
-#include "sig.h"
-#include "variables.h"
+#include "parser.h"
 #include <stdio.h>
 #include <readline/history.h>
 #include <readline/readline.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define PROMPT "minishell$ "
 
-void	init_shell(t_context *ctx, char **argv, char **envp);
-void	init_loop(t_context *ctx);
+void		init_shell(t_context *ctx, char **argv, char **envp);
+void		init_loop(t_context *ctx);
+
+static void	display_exit_message(t_context *ctx)
+{
+	if (ctx->is_interactive)
+	{
+		if (ctx->is_login)
+			ft_putendl_fd("logout", STDERR_FILENO);
+		else
+			ft_putendl_fd("exit", STDERR_FILENO);
+	}
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -39,7 +49,7 @@ int	main(int argc, char **argv, char **envp)
 		init_loop(&ctx);
 		line = readline(PROMPT);
 		if (line == NULL)
-			exit(EXIT_SUCCESS);
+			break ;
 		if (*line != '\0')
 		{
 			add_history(line);
@@ -50,4 +60,6 @@ int	main(int argc, char **argv, char **envp)
 		}
 		free(line);
 	}
+	display_exit_message(&ctx);
+	exit(ctx.last_exit_status);
 }
