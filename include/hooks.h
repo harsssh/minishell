c6dba6860162d9a89d3d5839910192401405f8a3
harsh <66548698+harsssh@users.noreply.github.com>
2023-12-03 01:46:53 +0900

    norm

--- include/hooks.h
+++ include/hooks.h
@@ -6,12 +6,12 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 00:45:13 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/12/03 00:46:52 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/12/03 01:46:49 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HOOKS_H
-#define HOOKS_H
+# define HOOKS_H
 
 int	sigint_event_hook(void);
 int	heredoc_sigint_event_hook(void);
