b68c379b655b96693bbe985ecdd2cb820657e4a8
EringiShimeji <eringi.shimeji.5296@gmail.com>
2023-12-03 16:16:47 +0900

    fix leaks

--- src/parser/parse.c
+++ src/parser/parse.c
@@ -6,15 +6,13 @@
 /*   By: smatsuo <smatsuo@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 20:01:43 by smatsuo           #+#    #+#             */
-/*   Updated: 2023/12/03 15:43:39 by smatsuo          ###   ########.fr       */
+/*   Updated: 2023/12/03 16:02:46 by smatsuo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "parser_internal.h"
 #include "ast.h"
-#include <stdio.h>
-#include "readline/readline.h"
 #include "token.h"
 #include "utils.h"
 
@@ -34,9 +32,9 @@ t_ast_node	*parse(const char *input, t_context *ctx)
 	result = parse_complete_command(parser);
 	if (result == NULL || !is_eof(parser))
 	{
-		if (get_stream(parser)->size == 0 && rl_catch_signals == 0)
+		if (get_stream(parser)->size == 0)
 			print_simple_error(ctx, "parser", "unexpected end of file");
-		else if (rl_catch_signals == 0)
+		else
 			print_syntax_error(ctx, parser);
 		ctx->last_exit_status = EXIT_SYNTAX_ERROR;
 		destroy_parser(parser);
