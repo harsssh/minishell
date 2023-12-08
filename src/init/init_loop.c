df63afe183b7b3eb7e8a52dc14e3f20a5a8f8b53
harsh <66548698+harsssh@users.noreply.github.com>
2023-12-09 06:40:59 +0900

    reset errno before each loop

--- src/init/init_loop.c
+++ src/init/init_loop.c
@@ -6,15 +6,17 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 01:06:59 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/10/30 01:07:28 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/12/09 06:39:54 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "init.h"
 #include "sig.h"
+#include <errno.h>
 
 void	init_loop(t_context *ctx)
 {
+	errno = 0;
 	set_shell_input_sig_handlers();
 	if (g_sig != 0)
 	{
