0e522e0c2ac12f68a6eb71831bf9af56b47a4040
EringiShimeji <eringi.shimeji.5296@gmail.com>
2023-12-08 20:51:22 +0900

    add heredoc test

--- e2e_tests/run.sh
+++ e2e_tests/run.sh
@@ -3,21 +3,39 @@
 cd "$(dirname "$0")"
 
 # テストケースをtestsディレクトリから取得して、拡張子を除いたファイル名のリストを作成
-tests=$(find tests -type f -name '*.in' | sort | sed -e 's/\..*$//')
+tests=$(find tests -type f -name '*.in' -or -name '*.err' | sort)
+is_failed=0
 
 for test in $tests
 do
-    # testsを先頭から削除した文字列をtest_nameに代入
-    test_name=$(echo $test | sed -e 's/^tests\///')
+    # testsを先頭から削除して、拡張子を除いた文字列をtest_nameに代入
+    test_name=$(echo $test | sed 's/^tests\///' | sed 's/\.[^.]*$//')
+    # 拡張子を取得して、.errか判定してis_err_caseに代入
+    is_err_case=$(echo $test | grep -q '\.err$' && echo 1 || echo 0)
     test_dir=$(dirname $test_name)
+
     mkdir -p diff/$test_dir
-    printf "Running test \033[33m$test_name\033[m\n"
-    ../minishell < $test.in > diff/$test_name.out
-    diff -u --color $test.expected diff/$test_name.out >&2
-    if [ $? -eq 0 ]
+
+    ../minishell < $test &> diff/$test_name.out
+    exit_status=$?
+    diff -u --color tests/$test_name.expected diff/$test_name.out > /dev/null
+    is_diff=$?
+
+    if [[ $is_diff -ne 0 || ($is_err_case -eq 1 && $exit_status -eq 0) ]]
     then
-        printf "Test $test \033[32mpassed\033[m\n"
+        is_failed=1
+        printf "$test \033[31mfailed\033[m\n" >&2
+        if [ $is_err_case -eq 1 -a $exit_status -eq 0 ]
+        then
+            printf "  └── exit status: expected non-zero, but got 0\n" >&2;
+        fi
+        if [ $is_diff -ne 0 ]
+        then
+            diff -u --color tests/$test_name.expected diff/$test_name.out
+        fi
     else
-        printf "Test $test \033[31mfailed\033[m\n" >&2
+        printf "$test \033[32mpassed\033[m\n"
     fi
 done
+
+exit $is_failed
--- src/parser/here_doc.c
+++ src/parser/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: smatsuo <smatsuo@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 18:19:43 by smatsuo           #+#    #+#             */
-/*   Updated: 2023/12/03 17:00:15 by smatsuo          ###   ########.fr       */
+/*   Updated: 2023/12/08 20:45:11 by smatsuo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,6 +81,25 @@ static t_redirect	*terminate_heredoc(int fd, char *line, t_redirect *redirect)
 	return (redirect);
 }
 
+static char	*read_command_line(t_context *ctx)
+{
+	char	*line;
+	char	*tmp;
+
+	if (ctx->is_interactive)
+		line = readline("> ");
+	else
+	{
+		line = get_next_line(STDIN_FILENO);
+		if (line == NULL)
+			return (NULL);
+		tmp = line;
+		line = ft_strtrim(tmp, "\n");
+		free(tmp);
+	}
+	return (line);
+}
+
 static t_redirect	*new_here_doc(char *delimiter, t_context *ctx)
 {
 	t_redirect	*redirect;
@@ -93,7 +112,7 @@ static t_redirect	*new_here_doc(char *delimiter, t_context *ctx)
 		return (destroy_redirect_and_return_null(redirect));
 	while (1)
 	{
-		line = readline("> ");
+		line = read_command_line(ctx);
 		if (line == NULL || g_sig == SIGINT)
 			return (terminate_heredoc(fd, line, redirect));
 		if (ft_strcmp(delimiter, line) == 0)
